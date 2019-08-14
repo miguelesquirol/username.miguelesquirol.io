c2d787f91d32cf43357d6534d6d374b078db8012
miguel <miguelesquirol@gmail.com>
2019-08-13 23:04:30 -0400

    Translations

--- src/scripts/components/translation.js
+++ src/scripts/components/translation.js
@@ -10,33 +10,92 @@ i18next.init({
     pt: {
       translation: {
         "title": "DIRETOR ARTÍSTICO",
-        "text" : "Como designer em design gráfico digital e tradicional, desenvolvo conceitos de diferentes formas e realizo projetos com o desejo constante de trazer resultados. Exigente, criativo, unificador, comprometido, sou excelente em encontrar cada vez o que o cliente mais precisa. Branding, publicação, postagem, websites, microsites, banners, CRM, redes sociais ... Com facilidade em gerenciamento de projetos e coaching de uma equipe, minha paixão está nos brainstorms coletivos e na minha força no busca de conceitos.",
-        "logo1" : "Glucerna-pt",
+        "text" : "<p>Como conceptor em design gráfico digital e tradicional, construo 360 campanhas e realizo vários projetos com o objetivo constante de trazer resultados.</p><p>Seja qual for o meio, minha expertise está no entendimento das reais necessidades do cliente e na minha força na capacidade de criar um conceito que responda a ele, em todos os pontos de contato que a marca possa ter com o seu target.</p><p></p>Vontade, criatividade e atenção aos detalhes são meus pontos fortes. Eu tenho o desejo constante de reunir as pessoas ao meu redor e todos os talentos da equipe para alcançar o melhor resultado possível. Costumava trabalhar em prazos apertados e para compensar os contratempos, não perco o rumo e estou sempre pronto para novos desafios!</p>",
+        "logo1" : "Glucerna",
         "logo1-text" : "<li>Refonte du site web</li><li>Expérience Usager</li><li>Prise de vue en studio</li><li>Montage photo</li>",
-        "logo2" : "Brunet - pt",
-        "logo2-text" : "Réseaux sociaux"
-
+        "logo2" : "Brunet",
+        "logo2-text" : "Réseaux sociaux",
+        "logo3" : "Metro",
+        "logo3-text" : "<li>Plateforme d’identité visuelle</li><li>Guide de normes graphiques</li><li>Livre de marque</li>",
+        "logo4" : "Fromagerie Bergeron",
+        "logo4-text" : "<li>Vidéo promotionnelle</li>",
+        "logo5" : "Similac",
+        "logo5-text" : "<li>Refonte du site web</li><li>Expérience Usager</li><li>Illustrations originales</li><li>Montage photo</li><li>Réseaux sociaux</li>",
+        "logo6" : "Formula 1",
+        "logo6-text" : "<li>Design de logos</li><li>Produits dérivés</li><li>Production québécoise</li>",
+        "logo7" : "STM",
+        "logo7-text" : "<li>Campagnes 360</li><li>Expérience client</li>",
+        "logo8" : "Mikes",
+        "logo8-text" : "<li>Campagnes numériques</li><li>Bannières web</li>",
+        "logo9" : "Ensure",
+        "logo9-text" : "<li>Refonte du site web</li><li>User Experience</li><li>Montage photo</li>",
+        "logo10" : "CSJR",
+        "logo10-text" : "<li>Refonte de l'identité visuelle</li><li>Site web</li><li>Affichage, dépliants et invitations</li>",
+        "logo11" : "La Presse",
+        "logo11-text" : " <li> Mise en page sur tablette</li><li>Illustrations originales</li><li>Data visuel, infographies</li><li>Expérience Usager</li>", 
+        "logo12" : "Bombardier",
+        "logo12-text" : "<li>Encarts publicitaires imprimés </li><li>Carte de Noël</li>"
       }
     },
     fr: {
       translation: {
         "title": "DIRECTRICE ARTISTIQUE",
-        "text": "Conceptrice en design graphique numérique et traditionnel, je développe des concepts sous différentes formes et réalise des projets avec le souci constant d’amener des résultats. Exigeante, créative, rassembleuse, engagée, j'excelle à trouver chaque fois ce dont le client a le plus besoin. Branding, édition, affichage, sites internet, microsites, bannières, CRM, réseaux sociaux... À l’aise dans la gestion de projet et l’encadrement d’une équipe, ma passion est dans les brainstorms collectifs et ma force dans la recherche de concepts.",
+        "text": "<p>Conceptrice à l'aise autant en numérique qu'en traditionnel, je construis des identités de marque, des expériences sur des lieux, des campagnes 360 et tout autre projet graphique avec le but constant de produire des résultats.</p><p>Quel que soit le support choisi, mon talent se révèle dans la compréhension des besoins réels du client et ma force dans la capacité de créer un concept qui y répond, dans tous les points de contact que la marque peut avoir avec sa cible.</p><p>La volonté, la créativité et le souci du détail sont mes points forts. J'ai le désir constant de réunir les gens autour de moi et tous les talents de l'équipe pour obtenir le meilleur résultat possible. Habituée à travailler dans des délais serrés et à jongler avec les contretemps, je ne perds pas le cap et je suis toujours prête pour de nouveaux défis!</p>",
         "logo1" : "Glucerna",
         "logo1-text" : "<li>Refonte du site web</li><li>Expérience Usager</li><li>Prise de vue en studio</li><li>Montage photo</li>",
         "logo2" : "Brunet",
-        "logo2-text" : "Réseaux sociaux", 
+        "logo2-text" : "Réseaux sociaux",
+        "logo3" : "Metro",
+        "logo3-text" : "<li>Plateforme d’identité visuelle</li><li>Guide de normes graphiques</li><li>Livre de marque</li>",
+        "logo4" : "Fromagerie Bergeron",
+        "logo4-text" : "<li>Vidéo promotionnelle</li>",
+        "logo5" : "Similac",
+        "logo5-text" : "<li>Refonte du site web</li><li>Expérience Usager</li><li>Illustrations originales</li><li>Montage photo</li><li>Réseaux sociaux</li>",
+        "logo6" : "Formula 1",
+        "logo6-text" : "<li>Design de logos</li><li>Produits dérivés</li><li>Production québécoise</li>",
+        "logo7" : "STM",
+        "logo7-text" : "<li>Campagnes 360</li><li>Expérience client</li>",
+        "logo8" : "Mikes",
+        "logo8-text" : "<li>Campagnes numériques</li><li>Bannières web</li>",
+        "logo9" : "Ensure",
+        "logo9-text" : "<li>Refonte du site web</li><li>User Experience</li><li>Montage photo</li>",
+        "logo10" : "CSJR",
+        "logo10-text" : "<li>Refonte de l'identité visuelle</li><li>Site web</li><li>Affichage, dépliants et invitations</li>",
+        "logo11" : "La Presse",
+        "logo11-text" : " <li> Mise en page sur tablette</li><li>Illustrations originales</li><li>Data visuel, infographies</li><li>Expérience Usager</li>", 
+        "logo12" : "Bombardier",
+        "logo12-text" : "<li>Encarts publicitaires imprimés </li><li>Carte de Noël</li>"
       }
     },
     
     en: {
       translation: {
         "title": "ARTISTIC DIRECTOR",
-        "text": "As a designer in digital and traditional graphic design, I develop concepts in different forms and realize projects with the constant desire to bring results. Demanding, creative, unifying, committed, I excel in finding each time what the customer needs most. Branding, publishing, posting, websites, microsites, banners, CRM, social networks ... At ease in project management and coaching a team, my passion is in the collective brainstorms and my strength in the search for concepts.." ,
-        "logo1" : "Glucerna-en",
+        "text": "<p>As a conceptor in digital and traditional graphic design, I build 360 campaigns and carry out various projects with the constant aim of bringing results.</p><p>Whatever the medium is, my expertise is in the understanding of the client real needs and my strength in the ability to create a concept that responds to it, in all points of contact that the brand could have with its target.</p><p>Willingness, creativity and attention to detail are my strengths. I have the constant desire to bring people around me together and all the team's talents to achieve the best possible result. Used to work in tight deadlines and to juggle the setbacks, I do not lose the course and I am always ready for new challenges!</p>" ,
+        "logo1" : "Glucerna",
         "logo1-text" : "<li>Refonte du site web</li><li>Expérience Usager</li><li>Prise de vue en studio</li><li>Montage photo</li>",
-        "logo2" : "Brunet - en",
-        "logo2-text" : "Réseaux sociaux" 
+        "logo2" : "Brunet",
+        "logo2-text" : "Réseaux sociaux",
+        "logo3" : "Metro",
+        "logo3-text" : "<li>Plateforme d’identité visuelle</li><li>Guide de normes graphiques</li><li>Livre de marque</li>",
+        "logo4" : "Fromagerie Bergeron",
+        "logo4-text" : "<li>Vidéo promotionnelle</li>",
+        "logo5" : "Similac",
+        "logo5-text" : "<li>Refonte du site web</li><li>Expérience Usager</li><li>Illustrations originales</li><li>Montage photo</li><li>Réseaux sociaux</li>",
+        "logo6" : "Formula 1",
+        "logo6-text" : "<li>Design de logos</li><li>Produits dérivés</li><li>Production québécoise</li>",
+        "logo7" : "STM",
+        "logo7-text" : "<li>Campagnes 360</li><li>Expérience client</li>",
+        "logo8" : "Mikes",
+        "logo8-text" : "<li>Campagnes numériques</li><li>Bannières web</li>",
+        "logo9" : "Ensure",
+        "logo9-text" : "<li>Refonte du site web</li><li>User Experience</li><li>Montage photo</li>",
+        "logo10" : "CSJR",
+        "logo10-text" : "<li>Refonte de l'identité visuelle</li><li>Site web</li><li>Affichage, dépliants et invitations</li>",
+        "logo11" : "La Presse",
+        "logo11-text" : " <li> Mise en page sur tablette</li><li>Illustrations originales</li><li>Data visuel, infographies</li><li>Expérience Usager</li>", 
+        "logo12" : "Bombardier",
+        "logo12-text" : "<li>Encarts publicitaires imprimés </li><li>Carte de Noël</li>"
       }
     }
 
@@ -50,7 +109,7 @@ function updateContent() {
   document.getElementById('title').innerHTML = i18next.t('title');
   document.getElementById('text').innerHTML = i18next.t('text');
 
-  for (var i = 1; i <= 2; i++) {
+  for (var i = 1; i <= 12; i++) {
   document.getElementById('logo' + i).innerHTML = i18next.t('logo' + i);
   document.getElementById('logo' + i + '-text').innerHTML = i18next.t('logo' + i + '-text'); }
 }
