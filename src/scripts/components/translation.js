773deeea83e4b2b1b78f8c5a79670b0e736db0f8
miguel <miguelesquirol@gmail.com>
2019-09-03 00:44:50 -0400

    changes

--- src/scripts/components/translation.js
+++ src/scripts/components/translation.js
@@ -11,7 +11,7 @@ export function translation() {
       resources: {
         pt: {
           translation: {
-            title: "Diseñador Gráfico - Líder Creativo",
+            title: "Diseñador Gráfico <span>-</span> Líder Creativo",
             text:
               "<p>Há mais de 10 anos trabalhando em impressão e digital, tenho paixão por criar soluções e experiências originais e atraentes. Eu vejo meus projetos desde a concepção até a produção, sem comprometer a atenção aos detalhes.</p><p></p>Minha força está no entendimento das necessidades reais do cliente e na capacidade de criar um conceito que responda a ele, em todos os pontos de contato. Eu sempre trago as pessoas juntas como um líder verdadeiro e apaixonado</p>",
             logo4: "GLUCERNA",
@@ -77,7 +77,7 @@ export function translation() {
         },
         fr: {
           translation: {
-            title: "Designer Graphique - Directrice Artistique",
+            title: "Designer Graphique <span>-</span> Directrice Artistique",
             text:
               "<p>Avec plus de 10 ans d'expérience en impression et en numérique, je me passionne pour la création et la conception de solutions originales et convaincantes. Je dirige mes projets de l’idée à la production, dans le souci constant du détail.</p><p>Ma force réside dans la compréhension des besoins réels du client et dans la capacité de créer un concept qui y réponde, dans tous les points de contact de la marque.</p>",
             logo4: "GLUCERNA",
@@ -97,10 +97,10 @@ export function translation() {
               "<li><b>Restaurants</b></li><li>Campagnes numériques</li><li>Bannières web</li>",
             logo6: "CSJR",
             "logo6-text":
-              "<li><b>Engagement communautaire</b></li><li>Logotype</li><li>Site web</li><li>Matériel promotionnel</li>",
+              "<li><b>Communautaire</b></li><li>Logotype</li><li>Site web</li><li>Matériel promotionnel</li>",
             logo7: "LA PRESSE +",
             "logo7-text":
-              "<li><b>Journal quotidien québécois</b><li>Édition numérique</li><li>Data-visuel et infographies</li><li>Illustration originale</li>",
+              "<li><b>Quotidien québécois</b><li>Édition numérique</li><li>Data-visuel</li><li>Illustration originale</li>",
             logo8: "BOMBARDIER",
             "logo8-text":
               "<li><b>Aéronautique</b></li><li>Publicité imprimée</li><li>Carte de Noël</li>",
@@ -141,7 +141,7 @@ export function translation() {
         },
         en: {
           translation: {
-            title: "Graphic Designer - Artistic Director",
+            title: "Graphic Designer <span>-</span> Artistic Director",
             text:
               "<p>More than 10 years working in both print and digital, I have a passion for creating original compelling design solutions and experiences. I see my projects from conception to production, without compromising attention to detail.</p><p>My strenght is in the understanding of the client real needs and the ability to create a concept that responds to it, in all the points of contact. I always bring people together as a true and passionate leader.</p>",
             logo4: "GLUCERNA",
@@ -204,7 +204,7 @@ export function translation() {
         },
         es: {
           translation: {
-            title: "Diseñador Gráfico - Líder Creativo",
+            title: "Diseñador Gráfico <span>-</span> Líder Creativo",
             text:
               "<p>Más de 10 años trabajando tanto en impresión como en digital, me apasiona crear soluciones y experiencias de diseño originales y convincentes. Superviso los proyectos durante todo el proceso, sin comprometer la atención al detalle.</p> <p> Mi fortaleza está en la comprensión de las necesidades reales del cliente y la capacidad de crear un concepto que responda a ellas, en todos los puntos de contacto. Acerco a los talentos como una verdadera y apasionada líder.</p>",
               logo4: "GLUCERNA",
